f876d0962c537fdc5680fc572e1c9b538265dc39
Muhammad Mahad <mahadtxt@gmail.com>
2026-07-18 10:33:13 +0500

    test(node): isolate worker spawn assertions
    Signed-off-by: Muhammad Mahad <mahadtxt@gmail.com>

--- loxa-node/src/bootstrap/mod.rs
+++ loxa-node/src/bootstrap/mod.rs
@@ -3,8 +3,6 @@ mod diagnostics;
 mod paths;
 
 pub(crate) use builder::NodeBuilder;
-#[cfg(test)]
-pub(crate) use builder::{download_worker_spawn_count, reset_download_worker_spawn_count};
 pub use diagnostics::{
     emit_final_shutdown_diagnostic, install_daemon_diagnostics, DiagnosticsBootstrap,
 };
